a54712be035c1b783604adbdd32e4b3b0b18e90d
Weihang Li <weihang.li@tum.de>
2022-02-01 23:32:59 +0100

    remove build

--- CppND-Route-Planning/cmake-build-debug/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lwh/Desktop/Projekt/Cpp-Nano-Udacity/CppND-Route-Planning/thirdparty/googletest/googlemock
-# Build directory: /home/lwh/Desktop/Projekt/Cpp-Nano-Udacity/CppND-Route-Planning/cmake-build-debug/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- CppND-Route-Planning/cmake-build-debug/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lwh/Desktop/Projekt/Cpp-Nano-Udacity/CppND-Route-Planning/thirdparty/googletest/googletest
-# Build directory: /home/lwh/Desktop/Projekt/Cpp-Nano-Udacity/CppND-Route-Planning/cmake-build-debug/thirdparty/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
